a3e412e6b5b185d1660f3314e59efe2c6fb973f8
root <kokidanny15@gmail.com>
2024-01-19 13:16:40 +0300

    old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,4 @@
 -- lists all the bands with Glam rock
-SELECT band_name, COALESCE(split, 2020) - formed AS lifespan FROM
-metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
+SELECT band_name, IFNULL(split, 2020) - formed AS lifespan
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', style) ORDER BY lifespan DESC;
