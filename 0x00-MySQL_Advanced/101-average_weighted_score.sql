227d53b146627b88cf75efcc65ca3e5691cc0e14
root <kokidanny15@gmail.com>
2024-01-20 16:54:22 +0300

    avaerage weight

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,11 +1,17 @@
 -- This script creates a stored procedure
 DROP procedure IF EXISTS ComputeAverageWeightedScoreForUser;
-DELIMITER |
+DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser()
 BEGIN
-    UPDATE users
-   	SET average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id=users.id)
-	WHERE id=user_id;
-END;
-|
+    UPDATE users AS pips,
+		(SELECT pips.id, SUM(score * weight) / SUM(weight) AS X
+		FROM users AS pips
+		JOIN corrections AS n ON pips.id=n.user_id
+		JOIN projects AS p ON n.project_id=p.id
+		GROUP BY pips.id)
+		AS D
+		SET pips.average_score = D.X
+		WHERE pips.id = D.id;
+END
+$$
 DELIMITER ;
