91e113cb5197ae627898d1bc16273f3ef41ca4fc
root <kokidanny15@gmail.com>
2024-01-24 02:35:35 +0300

    top scorer

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Top student
+"""
+
+
+def top_students(mongo_collection):
+    """
+    returns all students sorted by average score
+    """
+    top_student = mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
+
+    return top_student
