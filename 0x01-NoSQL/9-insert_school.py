0bf40b3ed6d16be6e0670bf2b105ae3cdc27389c
root <kokidanny15@gmail.com>
2024-01-24 01:41:57 +0300

    insert data

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -8,5 +8,5 @@ def insert_school(mongo_collection, **kwargs):
     """
     inserts a new document in a collection based on kwargs
     """
-    data_id = mongo_collection.insert_one(**kwargs)
+    data_id = mongo_collection.insert(kwargs)
     return data_id
